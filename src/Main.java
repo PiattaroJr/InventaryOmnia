39da75ce22c51ca46ebda75579e9218ee548afb3
piattarojr <giovipec88@gmail.com>
2024-05-03 17:35:45 +0200

    project materassi first version

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
-            // commento da Andrea
-            // commento da giovanni
             public void run() {
                 //LoginView view = new LoginView();
                 provaView provaView = new provaView();

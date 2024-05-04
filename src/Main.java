007dd0ca3e8f82b8580b1863d00be5f6af4f68fc
piattarojr <giovipec88@gmail.com>
2024-05-04 12:24:05 +0200

    project materassi first version

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
+
             @Override
             public void run() {
                 //LoginView view = new LoginView();

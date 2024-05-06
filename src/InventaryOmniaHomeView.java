06b003c0db2a7b126d5f28c36e2ab79b7ae6e249
piattarojr <giovipec88@gmail.com>
2024-05-06 11:24:28 +0200

    project materassi first version

--- src/InventaryOmniaHomeView.java
+++ src/InventaryOmniaHomeView.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class InventaryOmniaHomeView {
+    private JPanel RootPanel;
+    private JPanel NorthPanel;
+    private JPanel CentralPanel;
+    private JButton button1;
+    private JButton button2;
 }

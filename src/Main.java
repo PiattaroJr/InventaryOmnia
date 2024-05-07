40945412ec71b03aceb9912b486f22c95bdb31a7
piattarojr <giovipec88@gmail.com>
2024-05-07 12:26:36 +0200

    bottoni belli cazzo

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-
             @Override
             public void run() {
                 // andrea
